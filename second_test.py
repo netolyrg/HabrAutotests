4ccfd12084a8d2916ca245c19e487abedd8653d9
abogdanov <netolyrg@gmail.com>
2022-07-28 20:16:52 +0300

    add second test, move set up and tear down to functions

--- second_test.py
+++ second_test.py
@@ -1,27 +1,11 @@
 import time
 
-from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+
+from first_try import setup, tear_down
 
 
-def setup():
-    print('set up')
-    driver = webdriver.WebDriver(executable_path='./chromedriver')
-
-    driver.get('https://habr.com')
-
-    time.sleep(1)
-
-    return driver
-
-
-def tear_down(driver):
-    print('tear down')
-    driver.quit()
-
-
-def test_basic_search(driver):
+def test_empty_search(driver):
     # поиск поля для поиска
     search_button_locator = By.XPATH, '//*[@data-test-id="search-button"]'
     search_button = driver.find_element(*search_button_locator)
@@ -31,7 +15,7 @@ def test_basic_search(driver):
     # вбить текст
     search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
     search_input = driver.find_element(*search_input_locator)
-    text_to_search = 'asdfasdfasdfasdf'
+    text_to_search = 'asdfasdfasdfasdfasdf'
     search_input.send_keys(text_to_search)
 
     # нажать на иконку поиска
@@ -44,21 +28,17 @@ def test_basic_search(driver):
     article_locator = By.TAG_NAME, 'article'
     articles = driver.find_elements(*article_locator)
     print(f'Number of articles is {len(articles)}')
-    time.sleep(1)
+    # проверяем текст
 
-    # посчитать количество страниц (50)
-    last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
-    last_page_number = driver.find_element(*last_page_locator)
-    element_text = last_page_number.text
-    print(f'Number of pages is {element_text}')
+    empty_res_locator = By.XPATH, '//*[@data-test-id="empty-placeholder-text"]'
+    empty_results = driver.find_element(*empty_res_locator)
+    print(f'Text on page: {empty_results.text}')
 
 
 if __name__ == '__main__':
     driver = setup()
 
-    try:
-        test_basic_search(driver)
-    except NoSuchElementException as error:
-        print(f'Test failed, reason: {error}')
+    test_empty_search(driver)
 
     tear_down(driver)
+
