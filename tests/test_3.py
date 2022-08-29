a03a78fcbae370b0afcfefc91660b02c7414d5e8
abogdanov <netolyrg@gmail.com>
2022-08-29 19:20:45 +0300

    add one todo, fix test for main page

--- tests/test_3.py
+++ tests/test_3.py
@@ -5,5 +5,5 @@ def test_main_page(driver):
     page = MainPage(driver)
     page.open()
 
-    assert page.count_articles_number() == 19
+    assert page.count_articles_number() == 20
     assert page.count_pages_number() == 50
