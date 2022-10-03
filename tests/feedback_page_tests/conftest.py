4277c2059152cc6cc0488c300520136fc165fd2e
abogdanov <netolyrg@gmail.com>
2022-10-03 20:30:13 +0300

    tests skeleton for feedback page

---
+++ tests/feedback_page_tests/conftest.py
@@ -0,0 +1,4 @@
+
+
+def feedback_page():
+    pass
---
+++ tests/feedback_page_tests/test_basics.py
@@ -0,0 +1,10 @@
+def test_errors_on_empty_fields(feedback_page):
+    pass
+
+
+def test_number_of_feedback_subject(feedback_page):
+    pass
+
+
+def test_change_subject(feedback_page):
+    pass
